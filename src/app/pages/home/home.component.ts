2c8d6363d2f1a4b0998a7e35e830b51aa00e7ced
Ahmed Maher <ahmedelseaidy22@gmail.com>
2023-12-13 01:26:33 +0200

    commiting css updates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,16 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  home_wallpaper: string =
-    '../../assets/images/home-wocintechchat-com-tYVkjjMYFBo-canva.jpg';
-  profile_wallpaper: string =
-    '../../assets/images/profile-wocintechchat-com-tYVkjjMYFBo-canva.jpg';
-  contact_wallpaper: string =
-    '../../assets/images/contact-wocintechchat-com-tYVkjjMYFBo-canva.jpg';
-  quote_wallpaper: string =
-    '../../assets/images/quote-wocintechchat-com-tYVkjjMYFBo-canva.jpg';
-  services_wallpaper: string =
-    '../../assets/images/services-wocintechchat-com-tYVkjjMYFBo-canva.jpg';
-
   ngOnInit(): void {}
 }
